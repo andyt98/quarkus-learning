0d2a07b5dde4fa281c8388ab695c1d4fae066c38
Andy-Costin Toma <toma.andy98@gmail.com>
2023-10-31 22:57:35 +0200

    quarkus testing

--- quarkus-testing/system-test-dev-mode.sh
+++ quarkus-testing/system-test-dev-mode.sh
@@ -11,25 +11,14 @@ cleanup() {
 # Trap EXIT signal to run the cleanup function
 trap cleanup EXIT
 
-# Go to the coffee-shop directory
 cd "${0%/*}"/coffee-shop || exit
-
-# Build the application
 mvn clean package -Dquarkus.package.type=mutable-jar
-
-# Build the Docker image
 docker build -t coffee-shop:mutable .
 
-# Return to the previous directory
 cd ..
-
-# Start up the services using docker-compose
 docker-compose -f system-test-dev-mode.yml up -d
 
-# Go back to the coffee-shop directory
 cd "${0%/*}"/coffee-shop || exit
-
-# Start the Quarkus remote development
 mvn quarkus:remote-dev \
   -Dquarkus.live-reload.url=http://localhost:8080 \
   -Dquarkus.live-reload.password=123 \
